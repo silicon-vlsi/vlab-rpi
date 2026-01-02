57e73146cd85c04e43782b7768e6562ef6f66dde
S Rout <saroj.rout@silicon.ac.in>
2026-01-02 17:47:23 +0530

    added bin

---
+++ bin/post-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+if [ $# -ne 1 ]
+then 
+  echo " "
+  echo "Usage: $0 <options>"
+  echo " "
+  echo "DESCRIPTION: Options are "
+  echo "  update:	Update the distro"
+  echo " "
+  exit 1
+fi
+
