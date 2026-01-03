1c89d817924bfa553f93c43327e7a86656478302
S Rout <saroj.rout@silicon.ac.in>
2026-01-03 15:43:35 +0530

    added iic scripts

--- bin/setup-osic.sh
+++ bin/setup-osic.sh
@@ -3,4 +3,4 @@
 cd /opt
 sudo git clone --depth 1 https://github.com/iic-jku/IIC-OSIC-TOOLS.git
 sudo chown -R root:users IIC-OSIC-TOOLS
-sudo chmod 775 IIC-OSIC-TOOLS
+sudo chmod 755 IIC-OSIC-TOOLS
